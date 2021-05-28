8f6ec2528e40f207b6b45da6eba04846c2f6bcad
Enzey <enzey@enzey.net>
2021-05-27 21:32:42 -0500

    Add basic welcome content

--- src/Client/App.svelte
+++ src/Client/App.svelte
@@ -1,11 +1,74 @@
 <script>
+    import Icon from 'svelte-awesome';
+    import {
+        faBars,
+    } from '@fortawesome/free-solid-svg-icons'
+
 	import RoutedContent from './RoutedContent.svelte'
+
+    let isNavExpanded = false;
 </script>
 
 <main class="warm">
+    <nav class="navbar navbar-expand-md">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="images/vrcon_MK3-11.png" alt="VRCon logo"/>
+            </a>
+
+            <button
+                class="navbar-toggler"
+                type="button"
+                aria-expanded="{isNavExpanded}"
+                aria-label="Toggle navigation"
+
+                on:click={() => {isNavExpanded = !isNavExpanded}}
+            >
+                <Icon data={faBars} scale="1"/>
+            </button>
+            <div class="collapse navbar-collapse {isNavExpanded ? 'show' : ''}" id="navbarNavAltMarkup">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/events/">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/staff/">Staff</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <RoutedContent />
 </main>
 
 <style>
+    nav.navbar {
+        background-color: var(--color-accent-3);
+        color: var(--color-web-white);
+    }
+    nav .container-fluid {
+        margin-bottom: -1.5em;
+    }
+    nav .navbar-collapse {
+        margin-top: -1.5em;
+    }
+    nav .navbar-toggler {
+        margin-top: -0.75em;
+    }
+    nav .navbar-collapse.show  {
+        margin-top: unset;
+        padding-bottom: 1.5em;
+    }
+
+    nav img {
+        height: 2.5em;
+    }
+    a.nav-link {
+        font-family: "Rubik", Roboto, sans-serif;
+    }
 
 </style>
--- src/Client/Components/Welcome.svelte
+++ src/Client/Components/Welcome.svelte
@@ -3,11 +3,22 @@
 
 <div class="guild-list">
     <h2>Welcome to VRCon</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3">
+                <img src="images/connieposter.png" class="img-fluid" alt="connie welcome">
+            </div>
+            <div class="col-sm-6 col-md-7 col-lg-8 col-xl-9">
+                <p>Thanks for stopping in</p>
+                <p>Our goal is to overcome the bounds of reality, and celebrate with entertainers and creators what makes VR so amazng! Let's work together to make the worlds most safe and accessible convention straight from the comfort of our own home!</p>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
     h2 {
-        margin: 2em 0 0 0;
+        margin: 1em 0;
         font-size: 32px;
         text-align: center;
     }
