478a89ca1318380ba3cea3cb89ea43d309339981
Enzey <enzey@enzey.net>
2021-05-27 22:49:33 -0500

    Link base url to Page.ja routing fix loading paths when a path exists in the url

--- src/Client/RoutedContent.svelte
+++ src/Client/RoutedContent.svelte
@@ -4,6 +4,14 @@
     import EventsList from './Components/EventsList.svelte'
     import StaffList from './Components/StaffList.svelte'
 
+    const baseUrl = document.querySelector('head base')?.getAttribute('href')
+    const isNestedPath = baseUrl && baseUrl === location.href
+
+    if (isNestedPath) {
+        const bathPath = window.location.pathname.slice(0, -1)
+        pageRouter.base(bathPath)
+    }
+
     let routeComponent = null;
     let routeParams = {};
     pageRouter('/', (ctx) => {
