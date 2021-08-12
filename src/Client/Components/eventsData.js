f2e4c26f54c76babac5e265650f270d3c0e20e3f
Enzey <enzey@enzey.net>
2021-08-12 12:24:09 -0500

    Add events list

---
+++ src/Client/Components/eventsData.js
@@ -0,0 +1,294 @@
+
+const data = [
+    {
+        start: new Date('2021-09-09T17:00:00-04:00'),
+        name: 'Opening Ceremony',
+    },
+    {
+        start: new Date('2021-09-09T18:00:00-04:00'),
+        name: 'Guided Tour',
+    },
+    {
+        start: new Date('2021-09-09T19:00:00-04:00'),
+        name: 'Muse Concert Series with Special Guests',
+    },
+    {
+        start: new Date('2021-09-09-T21:00:00-04:00'),
+        name: 'Science outreach in VR',
+    },
+    {
+        start: new Date('2021-09-09-T22:00:00-04:00'),
+        name: 'How to Combined ALL your VRC Worlds into ONE',
+    },
+    {
+        start: new Date('2021-09-09-T23:00:00-04:00'),
+        name: 'Opening Party?',
+    },
+    {
+        start: new Date('2021-09-10T00:00:00-04:00'),
+        name: 'Opening Party?',
+    },
+    {
+        start: new Date('2021-09-10T01:00:00-04:00'),
+        name: 'Opening Party?',
+    },
+    {
+        start: new Date('2021-09-10-T03:00:00-04:00'),
+        name: 'hangout',
+    },
+    {
+        start: new Date('2021-09-10-T04:00:00-04:00'),
+        name: 'film festival watch party',
+    },
+    {
+        start: new Date('2021-09-10-T05:00:00-04:00'),
+        name: 'worldhop',
+    },
+    {
+        start: new Date('2021-09-10-T06:00:00-04:00'),
+        name: 'Putt Putt Pond',
+    },
+    {
+        start: new Date('2021-09-10-T07:00:00-04:00'),
+        name: 'Climbing',
+    },
+    {
+        start: new Date('2021-09-10-T08:00:00-04:00'),
+        name: 'casual pool',
+    },
+    {
+        start: new Date('2021-09-10-T00:00:00-04:00'),
+        name: 'Commmunity Spotlight 𝗪𝗮𝗶𝗳𝘂𝘄𝗲𝗵𝗿',
+    },
+    {
+        start: new Date('2021-09-10-T10:00:00-04:00'),
+        name: 'No Friends Friends Club',
+    },
+    {
+        start: new Date('2021-09-10-T11:00:00-04:00'),
+        name: 'Tygre, The Singing Furry',
+    },
+    {
+        start: new Date('2021-09-10-T13:00:00-04:00'),
+        name: 'New Player Meetup / Mini Intro To VRChat',
+    },
+    {
+        start: new Date('2021-09-10-T14:00:00-04:00'),
+        name: 'World Hopping w/ New Players',
+    },
+    {
+        start: new Date('2021-09-10-T14:00:00-04:00'),
+        name: 'Pool Tournament',
+    },
+    {
+        start: new Date('2021-09-10-T16:00:00-04:00'),
+        name: 'Dance Cypher',
+    },
+    {
+        start: new Date('2021-09-10-T19:00:00-04:00'),
+        name: 'LGBT+ in VRC',
+    },
+    {
+        start: new Date('2021-09-10-T20:00:00-04:00'),
+        name: 'Quiz Show!',
+    },
+    {
+        start: new Date('2021-09-10-T21:00:00-04:00'),
+        name: 'What Is VRCRoleplay?',
+    },
+    {
+        start: new Date('2021-09-10-T22:00:00-04:00'),
+        name: 'VRC Roleplay: Let's Get Improvisational!',
+    },
+    {
+        start: new Date('2021-09-10-T23:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-11-T00:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-11-T01:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-11-T03:00:00-04:00'),
+        name: 'Muder Game',
+    },
+    {
+        start: new Date('2021-09-11-T04:00:00-04:00'),
+        name: 'Quest World Hop',
+    },
+    {
+        start: new Date('2021-09-11-T05:00:00-04:00'),
+        name: 'Citrus0range's Warhammer Avatar PVP Showcase/Battle',
+    },
+    {
+        start: new Date('2021-09-11-T06:00:00-04:00'),
+        name: 'community spotlight SCP',
+    },
+    {
+        start: new Date('2021-09-11-T07:00:00-04:00'),
+        name: 'community spotlight: LPD',
+    },
+    {
+        start: new Date('2021-09-11-T08:00:00-04:00'),
+        name: 'Storytime with Magnanix',
+    },
+    {
+        start: new Date('2021-09-11-T09:00:00-04:00'),
+        name: 'Film Festival Watch Party',
+    },
+    {
+        start: new Date('2021-09-11-T10:00:00-04:00'),
+        name: 'Finding Your Voice in VR',
+    },
+    {
+        start: new Date('2021-09-11-T11:00:00-04:00'),
+        name: 'Deaf Culture Panel',
+    },
+    {
+        start: new Date('2021-09-11-T12:00:00-04:00'),
+        name: 'Saturday Squats',
+    },
+    {
+        start: new Date('2021-09-11-T13:00:00-04:00'),
+        name: 'IM_NAKU LIVE',
+    },
+    {
+        start: new Date('2021-09-11-T14:00:00-04:00'),
+        name: 'AdzySlayer Live Performance!',
+    },
+    {
+        start: new Date('2021-09-11-T14:30:00-04:00'),
+        name: 'March with the Kitten Marching Band',
+    },
+    {
+        start: new Date('2021-09-11-T15:00:00-04:00'),
+        name: 'Laserdome Starts',
+    },
+    {
+        start: new Date('2021-09-11-T16:00:00-04:00'),
+        name: 'Laserdome',
+    },
+    {
+        start: new Date('2021-09-11-T16:00:00-04:00'),
+        name: 'Open RP for New comers',
+    },
+    {
+        start: new Date('2021-09-11-T17:00:00-04:00'),
+        name: 'Laserdome Ends',
+    },
+    {
+        start: new Date('2021-09-11-T18:00:00-04:00'),
+        name: 'Improv Workshop',
+    },
+    {
+        start: new Date('2021-09-11-T19:00:00-04:00'),
+        name: 'VRCon Talent Show',
+    },
+    {
+        start: new Date('2021-09-11-T21:00:00-04:00'),
+        name: 'What's in the box?',
+    },
+    {
+        start: new Date('2021-09-11-T21:00:00-04:00'),
+        name: 'VR Pole Dancing - Next level of fitness',
+    },
+    {
+        start: new Date('2021-09-11-T22:00:00-04:00'),
+        name: 'VRTravellers Presents: The Pilot',
+    },
+    {
+        start: new Date('2021-09-11-T23:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-12-T00:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-12-T01:00:00-04:00'),
+        name: 'Party?',
+    },
+    {
+        start: new Date('2021-09-12-T03:00:00-04:00'),
+        name: 'Would you rather',
+    },
+    {
+        start: new Date('2021-09-12-T04:00:00-04:00'),
+        name: 'Freeze Tag',
+    },
+    {
+        start: new Date('2021-09-12-T05:00:00-04:00'),
+        name: 'Werewolf',
+    },
+    {
+        start: new Date('2021-09-12-T06:00:00-04:00'),
+        name: 'Quest Mini Golf',
+    },
+    {
+        start: new Date('2021-09-12-T08:00:00-04:00'),
+        name: 'VR dance showcase by the SXSW2021 champion YOIKAMI',
+    },
+    {
+        start: new Date('2021-09-12-T09:00:00-04:00'),
+        name: 'Community Spotlight: Kitten Marching Band',
+    },
+    {
+        start: new Date('2021-09-12-T10:00:00-04:00'),
+        name: 'Explore VRChat World Hop',
+    },
+    {
+        start: new Date('2021-09-12-T11:00:00-04:00'),
+        name: 'Find your Voice: voice training workshop',
+    },
+    {
+        start: new Date('2021-09-12-T13:00:00-04:00'),
+        name: 'Introduction to VR Sign Language',
+    },
+    {
+        start: new Date('2021-09-12-T14:00:00-04:00'),
+        name: 'Film Festival Watch Party',
+    },
+    {
+        start: new Date('2021-09-12-T14:00:00-04:00'),
+        name: 'The Amazing Spider-Lair Experience',
+    },
+    {
+        start: new Date('2021-09-12-T15:00:00-04:00'),
+        name: 'VRCLens Filmmaking & Photography Workshop',
+    },
+    {
+        start: new Date('2021-09-12-T16:00:00-04:00'),
+        name: 'Meme showcase 2',
+    },
+    {
+        start: new Date('2021-09-12-T17:00:00-04:00'),
+        name: 'MrCreepyPasta’s Storytime',
+    },
+    {
+        start: new Date('2021-09-12-T18:00:00-04:00'),
+        name: 'VRChat Movie Panel',
+    },
+    {
+        start: new Date('2021-09-12-T19:00:00-04:00'),
+        name: 'Boneworks dev talk on future of VR',
+    },
+    {
+        start: new Date('2021-09-12-T20:00:00-04:00'),
+        name: 'Belly Dance Lesson',
+    },
+    {
+        start: new Date('2021-09-12-T21:00:00-04:00'),
+        name: 'Improv Show',
+    },
+    {
+        start: new Date('2021-09-12-T20:00:00-04:00'),
+        name: 'Closing Ceremony',
+    },
+
+]
+
+export default data;
