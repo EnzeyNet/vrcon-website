6d86dc1845bb0f8cd56a518e1f23504f3befcc6c
Xanir <support@enzey.net>
2021-09-08 16:23:50 -0500

    Add 2021 worlds Update world naming format

--- src/Client/Components/WorldListCard.svelte
+++ src/Client/Components/WorldListCard.svelte
@@ -4,31 +4,51 @@
 <div class="Worlds">
     <div class="container">
         <h2>World List</h2>
-<!--->
+    </div>
+
+    <div class="container">
         <h3>2021</h3>
         <div class="WorldList">
+            <a class="WorldCard" href="https://vrch.at/wrld_9e455e2d-9536-40dc-a36c-a13be3838dca">
+                <img alt="VRCon Hub" src="images/Worlds/2021-vrcon-hub.png" />
+                <div class="WorldName">VRCon 2021 Hub</div>
+            </a>
+            <a class="WorldCard" href="https://vrch.at/wrld_320cdd4c-219d-4c06-82d5-0fb469eec248">
+                <img alt="VRCon Community Festival" src="images/Worlds/2021-vrcon-community-festival.png" />
+                <div class="WorldName">VRCon 2021 Community Festival</div>
+            </a>
+            <a class="WorldCard" href="https://vrch.at/wrld_cdbeebf8-f854-471d-9f2c-b3d7a76095a6">
+                <img alt="VRCon Tree Party Stage" src="images/Worlds/2021-vrcon-tree-party-stage.png" />
+                <div class="WorldName">VRCon 2021 Tree Party Stage</div>
+            </a>
+            <a class="WorldCard" href="https://vrch.at/wrld_95221a92-9fa5-4a68-9f9d-c94d9ec20a72">
+                <img alt="VRCon Confrence Room" src="images/Worlds/2021-vrcon-cave-theater.png" />
+                <div class="WorldName">VRCon 2021 Cave Theater</div>
+            </a>
         </div>
-<--->
+    </div>
+
+    <div class="container">
         <h3>2020</h3>
         <div class="WorldList">
             <a class="WorldCard" href="https://vrch.at/wrld_da7b0546-0855-492f-a910-9562d255e019">
-                <img alt="VRCon Confrence Room" src="images/Worlds/005.png" />
+                <img alt="VRCon Confrence Room" src="images/Worlds/2020-vrcon-confrence-room.png" />
                 <div class="WorldName">VRCon Confrence Room</div>
             </a>
             <a class="WorldCard" href="https://vrch.at/wrld_ba4a03a2-26dd-4597-a5a5-924d795c4279">
-                <img alt="VRCon Billards" src="images/Worlds/003.png" />
+                <img alt="VRCon Billards" src="images/Worlds/2020-vrcon-billards.png" />
                 <div class="WorldName">VRCon Billards</div>
             </a>
             <a class="WorldCard" href="https://vrch.at/wrld_56b13c26-6eef-4bfa-8f56-bfbb0e44e76a">
-                <img alt="VRCon 2020 Day" src="images/Worlds/001.png" />
+                <img alt="VRCon 2020 Day" src="images/Worlds/2020-vrcon-day.png" />
                 <div class="WorldName">VRCon 2020 Day</div>
             </a>
             <a class="WorldCard" href="https://vrch.at/wrld_a182db1b-0b18-4be3-b40b-5d720c7c21b6">
-                <img alt="VRCon 2020 Night" src="images/Worlds/002.png" />
+                <img alt="VRCon 2020 Night" src="images/Worlds/2020-vrcon-night.png" />
                 <div class="WorldName">VRCon 2020 Night</div>
             </a>
             <a class="WorldCard" href="https://vrch.at/wrld_e32e0265-d3a4-4482-8685-a96a34e4dc06">
-                <img alt="VRCon 2020 Stage" src="images/Worlds/004.png" />
+                <img alt="VRCon 2020 Stage" src="images/Worlds/2020-vrcon-stage.png" />
                 <div class="WorldName">VRCon 2020 Stage</div>
             </a>
         </div>
