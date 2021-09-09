cf6924a3229a2df0585d2cd3b070e71ab4ca7624
Xanir <support@enzey.net>
2021-09-09 16:15:28 -0500

    Redirect from http to https

--- src/Client/main.js
+++ src/Client/main.js
@@ -1,5 +1,14 @@
 import App from './App.svelte';
 
+if (location?.protocol?.indexOf('http:') === 0) {
+    if (location?.hostname !== 'localhost') {
+        location.replace('https://' + location.hostname + location.pathname)
+        console.log('HTTP to HTTPS redirect running')
+    } else {
+        console.log('not redirecting dev to https')
+    }
+}
+
 const app = new App({
 	target: document.body,
 	props: {
